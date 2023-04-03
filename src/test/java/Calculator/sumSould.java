55711fbdfabd1b606147a49d7d794f37b8b55bfd
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-03 11:05:01 +0100

    🔴 a_empty_entry_results_0

---
+++ src/test/java/Calculator/sumSould.java
@@ -0,0 +1,21 @@
+package Calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+"" ->0
+"1"->1
+"1,2"->3
+ */
+
+class sumSould {
+    @Test
+
+    void foo(){
+        assertEquals("0",sum(""));
+    }
+
+
+
+}
