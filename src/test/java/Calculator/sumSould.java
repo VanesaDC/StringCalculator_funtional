069545170c9e448634200e75907c900b64449884
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-04 10:14:50 +0100

    🟢allow_support_different_delimiter

--- src/test/java/Calculator/sumSould.java
+++ src/test/java/Calculator/sumSould.java
@@ -2,8 +2,6 @@ package Calculator;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /*
@@ -16,28 +14,60 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class sumSould {
 
-    public int add(String numberSeries) {
+    public int add(String entry) {
 
-        if (numberSeries.isEmpty()){
+        if (entry.isEmpty()){
             return 0;
         }
 
-        boolean isAUniqueNumber = numberSeries.length() == 1;
+        boolean isAUniqueNumber = entry.length() == 1;
         if (isAUniqueNumber){
-            return Integer.parseInt(numberSeries);
+            return Integer.parseInt(entry);
+        }
+        if (entry.contains("//")){
+            int indexForSplit = positionOfFirthsDigit(entry);
+            String numberSeries = entry.substring(indexForSplit);
+            String delimiter = entry.substring(2,indexForSplit).trim();
+            delimiter = delimiter.replace("[","");
+            delimiter = delimiter.replace("]","");
+            numberSeries = numberSeries.replace(delimiter,",");
+            String[] numbers = numberSeries.split(",");
+            int sum = 0;
+            for (String number:
+                    numbers) {
+                sum += Integer.parseInt(number);
+            }
+            return sum;
+
         }
 
-        if (numberSeries.contains("\n")){
-            numberSeries = numberSeries.replace("\n",",");
+        if (entry.contains("\n")){
+            entry = entry.replace("\n",",");
         }
 
-        String[] numbers = numberSeries.split(",");
-        int sum=0;
+        String[] numbers = entry.split(",");
+        int sum = 0;
         for (String number:
              numbers) {
-            sum+=Integer.parseInt(number);
+            sum += Integer.parseInt(number);
         }
         return sum;
+    }
+
+    private int positionOfFirthsDigit(String text) {
+        int place = -1;
+        char caracter;
+        int position=0;
+        while(place==-1){
+
+            caracter=text.charAt(position);
+            if (Character.isDigit(caracter)){
+                place=position;
+            }else{
+                position++;
+            }
+        }
+        return place;
     }
 
 
@@ -64,7 +94,7 @@ class sumSould {
     }
     @Test
     void allow_support_different_delimiter(){
-        assertEquals(3, add("//[;]\n1;3"));
+        assertEquals(3, add("//[;]\n1;2"));
     }
 
 
