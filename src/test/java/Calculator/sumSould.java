cfb4fcdca439ac4e15bb57cd874b7844e5ee5e40
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-05 08:58:43 +0100

    🟢 throw_exception_if_entry_contain_negative_number

--- src/test/java/Calculator/sumSould.java
+++ src/test/java/Calculator/sumSould.java
@@ -1,8 +1,8 @@
 package Calculator;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 /*
 "" ->0
@@ -15,114 +15,91 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class sumSould {
 
-    public int add(String entry) {
+    public int add(String entry) throws NegativeException {
 
-        if (entry.isEmpty()){
+        if (entry.isEmpty()) {
             return 0;
         }
 
         boolean isAUniqueNumber = entry.length() == 1;
-        if (isAUniqueNumber){
+        if (isAUniqueNumber) {
             return Integer.parseInt(entry);
         }
-        if (entry.contains("//")){
-            int indexForSplit = positionOfFirthsDigit(entry);
-            String numberSeries = entry.substring(indexForSplit);
-            String delimiter = entry.substring(2,indexForSplit).trim();
-            delimiter = delimiter.replace("[","");
-            delimiter = delimiter.replace("]","");
-            numberSeries = numberSeries.replace(delimiter,",");
 
-            int total=0;
-            try {
-                total= resultOfSum(numberSeries);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return total;
-
+        String newDelimiterCommand = "//";
+        if (entry.contains (newDelimiterCommand)) {
+            entry = setNewDelimiter(entry);
         }
 
-        if (entry.contains("\n")){
-            entry = entry.replace("\n",",");
+        String newLine = "\n";
+        if (entry.contains(newLine)) {
+            entry = entry.replace(newLine, ",");
         }
 
-        int total=0;
-        try {
-            total= resultOfSum(entry);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return total;
-
-    }
-
-    private static int resultOfSum(String entry) throws NegativeException{
         String[] numbers = entry.split(",");
         int sum = 0;
-        for (String number:
-             numbers) {
-            if (Integer.parseInt(number)<0){
-                throw new NegativeException ("Error: Negatives numbers not allwed");
-            }else{
+        for (String number :
+                numbers) {
+            if (Integer.parseInt(number) < 0) {
+                throw new NegativeException("Error: Negatives numbers not allowed " + number);
+            } else {
                 sum += Integer.parseInt(number);
             }
-
         }
-        return sum;
+
+        return sum ;
     }
 
-    private int positionOfFirthsDigit(String text) {
-        int place = -1;
-        char caracter;
-        int position=0;
-        while(place==-1){
-
-            caracter=text.charAt(position);
-            if (Character.isDigit(caracter)){
-                place=position;
-            }else{
-                position++;
-            }
-        }
-        return place;
+    private String setNewDelimiter(String entry) {
+        String[] entrySubdivisions= entry.split("\n",0);
+        String delimiter= entrySubdivisions[0].substring(2);
+        delimiter= delimiter.replace("[","");
+        delimiter = delimiter.replace("]", "");
+        entry = entrySubdivisions[1].replace(delimiter, ",");
+        return entry;
     }
+
 
 
 
     @Test
-    void empty_entry_results_0(){
+    void empty_entry_results_0() throws NegativeException {
         assertEquals(0, add(""));
     }
+
     @Test
-    void a_number_results_the_same_number(){
+    void a_number_results_the_same_number() throws NegativeException {
         assertEquals(1, add("1"));
     }
+
     @Test
-    void two_numbers_results_the_sum_of_the_numbers(){
+    void two_numbers_results_the_sum_of_the_numbers() throws NegativeException {
         assertEquals(3, add("1,2"));
     }
+
     @Test
-    void add_all_number_from_the_text(){
+    void add_all_number_from_the_text() throws NegativeException {
         assertEquals(10, add("1,2,3,4"));
     }
+
     @Test
-    void allow_new_line_between_numbers(){
+    void allow_new_line_between_numbers() throws NegativeException {
         assertEquals(6, add("1\n2,3"));
     }
+
     @Test
-    void allow_support_different_delimiter(){
-        assertEquals(3, add("//[;]\n1;2"));
-    }
-    @Test
-    void throw_exception_if_entry_contain_negatives_number(){
-        String errorMessage = "Error: Negatives numbers not allwed";
-        NegativeException exceptionCapture = assertThrows(NegativeException.class, ()->
-                add("1,-2"));
-        assertEquals(errorMessage,exceptionCapture.getMessage());
+    void allow_support_different_delimiter() throws NegativeException {
+        assertEquals(6, add("//[;]\n1;2;3"));
     }
 
 
-
+   @Test
+    void throw_exception_if_entry_contain_negatives_number() {
+        assertThatExceptionOfType(NegativeException.class).isThrownBy(() -> {
+            add("1,-2");
+        }).withMessage("Error: Negatives numbers not allowed -2");
+    }
 
 }
+
+
