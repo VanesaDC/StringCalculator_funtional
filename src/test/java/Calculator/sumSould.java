af14d9d28e1e5f9a49ebdd39ab8780ac821edf41
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-11 08:45:23 +0100

    🔴allow_new_line_between_numbers_instead_of_commas

--- src/test/java/Calculator/sumSould.java
+++ src/test/java/Calculator/sumSould.java
@@ -10,13 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
 "" ->0
 "1"->1
 "1,2"->3
+“1\n2,3”->6
  */
 
 class sumSould {
 
     public int add(String texto) {
-        boolean is_a_unique_number = texto.length() == 1;
 
+        boolean is_a_unique_number = texto.length() == 1;
         if (is_a_unique_number){
             return Integer.parseInt(texto);
         }
@@ -37,6 +38,10 @@ class sumSould {
     @Test
     void two_numbers_results_the_sum_of_the_numbers(){
         assertEquals(3, add("1,2"));
+    }
+    @Test
+    void allow_new_line_between_numbers_instead_of_commas(){
+        assertEquals(6, add("1\n2,3"));
     }
 
 
