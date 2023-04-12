e5db0af02349ac3c3f8c4a6f90de38a8768ac3c0
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-12 12:12:12 +0100

    🔵allow_different_delimiters

--- src/test/java/Calculator/sumSould.java
+++ src/test/java/Calculator/sumSould.java
@@ -27,25 +27,9 @@ class sumSould {
             return Integer.parseInt(entry);
         }
 
-
-        if (entry.contains("//")) { //
-
-            entry = entry.replace("//", "");
-            entry = entry.replace("[", "");
-            entry = entry.replace("]", "");
-            String delimiter= String.valueOf(entry.charAt(0));
-            char delimit = entry.charAt(0);
-            entry = entry.replaceFirst(String.valueOf(delimit), "");
-            entry= entry.replaceFirst("\n","");
-            entry= entry.replace(delimiter,",");
+        if (entry.contains("//")) {
+            entry = setDelimiter(entry);
         }
-
-
-            /*String numberSeries = entry.substring(0,"\n")
-            String delimiter = entry.substring(2,indexForSplit).trim();
-            delimiter = delimiter.replace("[","");
-            delimiter = delimiter.replace("]","");
-            numberSeries = numberSeries.replace(delimiter,",");*/
 
         String newLine = "\n";
         if (entry.contains(newLine)){
@@ -55,6 +39,18 @@ class sumSould {
         String[] numberSeries= entry.split(",");
         List<Integer> numbers= Arrays.stream(numberSeries).map((num)->Integer.parseInt(num)).collect(Collectors.toList());
         return numbers.stream().reduce(0,(subtotal, number)->subtotal + number);
+    }
+
+    private static String setDelimiter(String entry) {
+        entry = entry.replace("//", "");
+        entry = entry.replace("[", "");
+        entry = entry.replace("]", "");
+        String delimiter= String.valueOf(entry.charAt(0));
+        char delimit = entry.charAt(0);
+        entry = entry.replaceFirst(String.valueOf(delimit), "");
+        entry = entry.replaceFirst("\n","");
+        entry = entry.replace(delimiter,",");
+        return entry;
     }
 
 
