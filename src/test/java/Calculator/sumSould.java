9de53f549f6f76b4a5144693f545802df97acefd
Vanesa Díaz Concepción <studiosvanesa@gmail.com>
2023-04-12 14:35:59 +0100

    🟢Throw_a_exception_when_entry_contains_negative_number

--- src/test/java/Calculator/sumSould.java
+++ src/test/java/Calculator/sumSould.java
@@ -2,9 +2,12 @@ package Calculator;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class sumSould {
 
-    public int add(String entry) {
+    public int add(String entry) throws NegativeException {
         if (entry.isEmpty())
             return 0;
 
@@ -38,7 +41,15 @@ class sumSould {
         }
 
         String[] numberSeries= entry.split(",");
+
         List<Integer> numbers= Arrays.stream(numberSeries).map((num)->Integer.parseInt(num)).collect(Collectors.toList());
+        List<Integer> negatives= numbers.stream().filter(negative->negative<0).collect(Collectors.toList());
+        List<String> negativesLetter= negatives.stream().map(neg->neg.toString()).collect(Collectors.toList());
+
+        String negativesSentence= negativesLetter.stream().reduce("",(sentence,word)->sentence +" "+ word);
+        if (!negativesSentence.isEmpty()) {
+            throw new NegativeException("Error: negative numbers not allowed:" + negativesSentence);
+        }
         return numbers.stream().reduce(0,(subtotal, number)->subtotal + number);
     }
 
@@ -56,28 +67,30 @@ class sumSould {
 
 
     @Test
-    void empty_entry_results_0(){
+    void empty_entry_results_0()throws NegativeException {
         assertEquals(0, add(""));
     }
     @Test
-    void a_number_results_the_same_number(){
+    void a_number_results_the_same_number() throws NegativeException {
         assertEquals(1, add("1"));
     }
     @Test
-    void two_numbers_results_the_sum_of_the_numbers(){
+    void two_numbers_results_the_sum_of_the_numbers() throws NegativeException {
         assertEquals(3, add("1,2"));
     }
     @Test
-    void allow_new_line_between_numbers_instead_of_commas(){
+    void allow_new_line_between_numbers_instead_of_commas() throws NegativeException {
         assertEquals(6, add("1\n2,3"));
+
     }
     @Test
-    void allow_different_delimiters(){
+    void allow_different_delimiters() throws NegativeException {
         assertEquals(3, add("//[;]\n1;2"));
     }
     @Test
     void Throw_a_exception_when_entry_contains_negative_number(){
-       assertThrows(NegativeException.class,()-> add("1,-2,-3"));
+       String message= assertThrows(NegativeException.class,()->add("1,-2,-3")).getMessage();
+       assertEquals("Error: negative numbers not allowed: -2 -3",message);
     }
 
 
